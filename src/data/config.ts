f32ba7543e115bd8de9f5a6372b42f8d62512b28
zyvrix <ay2690357@gmail.com>
2025-11-06 16:22:39 +0530

    Initial commit - 3D-Interactive-portfolio

---
+++ src/data/config.ts
@@ -0,0 +1,48 @@
+const config = {
+  title: "Dinesh Yadav | Full-Stack Developer",
+  description: {
+    long: "Explore the portfolio of Dinesh Yadav, a passionate Full Stack Developer and Computer Science Engineer skilled in the MERN stack, DevOps, and modern web technologies. Discover his innovative projects, technical expertise, and hands-on experience in building scalable, user-friendly web applications.",
+    short:
+      "Discover the portfolio of Dinesh, a Full-Stack Developer solving real life problems in Web-Development.",
+  },
+  keywords: [
+    "Dinesh",
+    "Dinesh Yadav",
+    "dineshyadav",
+    "dineshyadav.com",
+    "Dinesh Yadav portfolio",
+    "portfolio",
+    "DevOps",
+    "Devlopment",
+    "Database",
+    "AI Docker file optimizer",
+    "Finance me",
+    "creative technologist",
+    "web development",
+    "interactive websites",
+    // "GSAP",
+    // "React",
+    // "Next.js",
+    // "Spline",
+    // "Framer Motion",
+    "AWS",
+    "DevSecOps",
+    "AIOps",
+    "Azure",
+  ],
+  author: "Dinesh Yadav",
+  email: "git.push.dinesh@gmail.com",
+  site: "https://abhijitzende.com",
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/Amityadv15",
+    linkedin: "https://www.linkedin.com/in/dineshyadav9256/",
+    instagram: "https://www.instagram.com/the_intr0vert__/",
+    facebook: "",
+    github: "https://github.com/zyvrix/",
+  },
+};
+export { config };
