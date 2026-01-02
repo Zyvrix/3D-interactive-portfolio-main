801e4440fcc65500d481e8cd613fdc00b981b969
Zyvrix <ay2690357@gmail.com>
2026-01-02 16:54:38 +0530

    fix erros

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
-export default nextConfig;
 
-module.exports = nextConfig;
+export default nextConfig;
