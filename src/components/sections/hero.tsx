827badeab61293ad4bd9a89bf4529317cf619a87
Zyvrix <ay2690357@gmail.com>
2026-01-03 23:08:01 +0530

    update resume link

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -83,7 +83,7 @@ const HeroSection = () => {
               <div className="mt-8 md:ml-2 flex flex-col gap-3">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1M0yb-BxDn9sgwb439OysdNCSEaoj1Yn6/view?usp=sharing"
+                    "https://drive.google.com/file/d/1D6LlFTrI6gaZfmeAUtUt9mvKQocnzqya/view?usp=sharing"
                   }
                   target="_blank"
                   className="flex-1"
